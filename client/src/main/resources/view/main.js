ab84b5e3c23b05330148e2eac04614dfd79f7487
Crap <422655094@qq.com>
2019-01-23 23:59:44 +0800

    提交html

--- client/src/main/resources/view/main.js
+++ client/src/main/resources/view/main.js
@@ -3,44 +3,35 @@ $script("lib/iview-3/iview.min.js", "iview");
 $script("lib/jquery/jquery-3.3.1.min.js", "jquery");
 $script("lib/common.js", "common");
 $script.ready(["vue", "iview", "jquery", "common"], function () {
+    $("#setting").load("module/setting/setting.html", {}, function () {
+        $script("module/setting/setting.js", "setting");
+    });
+    $("#contacts").load("module/contacts/contacts.html", {}, function () {
+        $script("module/contacts/contacts.js", "contacts");
+    });
+    $("#chat").load("module/chat/chat.html", {}, function () {
+        $script("module/chat/chat.js", "chat");
+    });
+    $("#keywords").load("module/keywords/keywords.html", {}, function () {
+        $script("module/keywords/keywords.js", "keywords");
+    });
+})
+
+$script.ready(["setting", "contacts", "chat", "keywords"], function () {
     new Vue({
-        el: '#app',
+        el: "#app",
         data: {
-            skin:"light",
-            data1: [
-                {
-                    title: 'parent 1',
-                    expand: true,
-                    children: [
-                        {
-                            title: 'parent 1-1',
-                            expand: true,
-                            children: [
-                                {
-                                    title: 'leaf 1-1-1'
-                                },
-                                {
-                                    title: 'leaf 1-1-2'
-                                }
-                            ]
-                        },
-                        {
-                            title: 'parent 1-2',
-                            expand: true,
-                            children: [
-                                {
-                                    title: 'leaf 1-2-1'
-                                },
-                                {
-                                    title: 'leaf 1-2-1'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
+            skin: "dark",
+            Setting: Setting.data,
+            Contacts: Contacts.data,
+            Chat: Chat.data,
+            Keywords: Keywords.data
         },
         methods: {
+            Setting: Setting.methods,
+            Contacts: Contacts.methods,
+            Chat: Chat.methods,
+            Keywords: Keywords.methods
         }
-    })
+    });
 })
