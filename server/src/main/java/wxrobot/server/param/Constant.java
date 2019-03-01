3b0c9db468f6c5cf90cde00c63a3ac0933ba1a11
Crap <422655094@qq.com>
2019-03-01 13:20:10 +0800

    提交

---
+++ server/src/main/java/wxrobot/server/param/Constant.java
@@ -0,0 +1,15 @@
+package wxrobot.server.param;
+
+public interface Constant {
+	
+	interface Timer {
+		
+		interface Type {
+			int 文本 = 1;
+			int 图片 = 2;
+			int 表情 = 3;
+			int 视频 = 4;
+			int 文件 = 5;
+		}
+	}
+}
