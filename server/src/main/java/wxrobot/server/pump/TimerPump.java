52d2f780f705a8a333aab0a68ea59fc3f95c90bc
Crap <422655094@qq.com>
2019-03-28 19:29:02 +0800

    提交修改

--- server/src/main/java/wxrobot/server/pump/TimerPump.java
+++ server/src/main/java/wxrobot/server/pump/TimerPump.java
@@ -1,5 +1,8 @@
 package wxrobot.server.pump;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,8 +17,10 @@ import org.crap.jrain.core.bean.result.Errcode;
 import org.crap.jrain.core.bean.result.criteria.Data;
 import org.crap.jrain.core.bean.result.criteria.DataResult;
 import org.crap.jrain.core.error.support.Errors;
+import org.crap.jrain.core.util.StringUtil;
 import org.crap.jrain.core.validate.annotation.BarScreen;
 import org.crap.jrain.core.validate.annotation.Parameter;
+import org.crap.jrain.mvc.netty.decoder.Part;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,16 +31,18 @@ import wxrobot.biz.server.TimerServer;
 import wxrobot.dao.entity.field.ScheduleMsg;
 import wxrobot.dao.entity.field.UserInfo;
 import wxrobot.server.enums.CustomErrors;
+import wxrobot.server.param.PartParam;
 import wxrobot.server.param.TokenParam;
+import wxrobot.server.sync.SyncContext;
 import wxrobot.server.sync.pojo.SyncAction;
 import wxrobot.server.sync.pojo.SyncBiz;
 import wxrobot.server.sync.pojo.SyncMsg;
 
 @Pump("timer")
 @Component
-public class TimerJSONPump extends DataPump<FullHttpRequest, Channel> {
+public class TimerPump extends DataPump<FullHttpRequest, Channel> {
 	
-	public static final Logger log = LogManager.getLogger(TimerJSONPump.class);
+	public static final Logger log = LogManager.getLogger(TimerPump.class);
 	
 	@Autowired
 	private TimerServer timerServer;
@@ -109,6 +116,71 @@ public class TimerJSONPump extends DataPump<FullHttpRequest, Channel> {
 //		return new DataResult(mod > 0?Errors.OK:CustomErrors.USER_OPR_ERR);
 //	}
 	
+	@Pipe("addMsg")
+	@BarScreen(
+		desc="添加分群定时消息",
+		params= {
+			@Parameter(type=TokenParam.class),
+			@Parameter(value="seq",  desc="seq"),
+			@Parameter(type=PartParam.class, value="content", desc="消息内容"),
+			@Parameter(value="schedule", desc="计划方案")
+		}
+	)
+	public Errcode addMsg (Map<?, ?> params) throws ErrcodeException {
+		
+		UserInfo userInfo = timerServer.getUserInfo(params);
+		String uuid = StringUtil.uuid();
+		ScheduleMsg msg = new ScheduleMsg();
+		msg.setUuid(uuid);
+		msg.setSchedule(params.get("schedule").toString());
+		Object content = params.get("content");
+		if(content instanceof Part){
+			Part part = (Part)content;
+			String ext = part.getFilename().substring(part.getFilename().lastIndexOf(".")+1);
+			String path = "attach/"+userInfo.getUserName() + "/"+ uuid+"."+ext;
+			String mimeType = null;
+			try {
+				part.write(path);
+				mimeType = Files.probeContentType(new File(path).toPath());
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			msg.setContent(uuid+"."+ext);
+			msg.setType(5);
+			if("gif".equals(ext))
+				msg.setType(3);
+			else if(mimeType != null){
+				switch (mimeType.split("/")[0]) {
+				case "image":
+					msg.setType(2);
+					break;
+				case "video":
+					msg.setType(4);
+					break;
+				}
+			}
+				
+		} else {
+			msg.setContent(content.toString());
+			msg.setType(1);
+		}
+		timerServer.addMsg(userInfo.getUserName(), params.get("seq").toString(), msg);
+		
+		//消息放入事件队列
+		SyncMsg event = new SyncMsg();
+		event.setBiz(SyncBiz.TIMER);
+		event.setAction(SyncAction.ADD);
+		
+		Map<String, Object> data = new HashMap<>();
+		data.put("seq", params.get("seq").toString());
+		data.put("timer", msg);
+		event.setData(data);
+		SyncContext.putMsg(params.get("token").toString(), event);
+				
+		return new DataResult(Errors.OK, new Data(msg));
+	}
+	
+	
 	@Pipe("delMsg")
 	@BarScreen(
 		desc="删除分群定时消息",
@@ -133,6 +205,7 @@ public class TimerJSONPump extends DataPump<FullHttpRequest, Channel> {
 			data.put("seq", params.getString("seq"));
 			data.put("uuid", params.getString("uuid"));
 			event.setData(data);
+			SyncContext.putMsg(params.getString("token"), event);
 		}
 		return new DataResult(mod > 0?Errors.OK:CustomErrors.USER_OPR_ERR);
 	}
